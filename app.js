5025f2f37fce9f03b7745610cecc812bead9d3a3
Kyara Hallet <kyara.hallet@gmail.com>
2018-08-13 16:31:28 +0200

    'h

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mysql = require('mysql'); 
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'views')));
 // Uncomment to external folders for routing
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -28,9 +28,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
+   let sql = `SELECT * FROM users
+           ORDER BY prenom`;
+ 
+  db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
   res.sendFile('index.html', { root: viewfolder});
 });
 
+app.get('/signup', function(req, res, next) {
+  prenom = req.query["prenom"]
+  nom = req.query["nom"]
+  email = req.query["mail"]
+  password = req.query["psw"]
+  year = req.query["annee"]
+  db.run(`INSERT INTO users(prenom,nom,email,password,year) VALUES(?,?,?,?,?)`, [prenom,nom,email,password,year], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
+  res.sendFile('index.html', { root: viewfolder})
+ });
+
+app.get('/login', function(req, res, next) {
+  email = req.query["mail"]
+  password = req.query["psw"]
+  db.all('SELECT COUNT(*) count FROM users WHERE email=? AND password=?',[email,password], function(err,rows) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    if(rows[0].count==0){
+    	console.log("not found")
+    }
+  });
+  res.sendFile('index.html', { root: viewfolder})
+ });
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('mydb');
+
+
+db.run('CREATE TABLE IF NOT EXISTS users(prenom text,nom text,email text,password text,year text)');
 
 ////////////////////////////start the server/////////////////////
 var port=3000
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
 });
 
 router.get('/nouvelle_annonce', function(req, res, next) {
   objet = req.query["objet"]
   res.send(objet)
 });
+
+router.get('/signup', function(req, res, next) {
+  objet = req.query["name"]
+  res.send(objet)
+});
+
+
 
 module.exports = router;
